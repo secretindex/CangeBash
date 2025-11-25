9b04ff5c374c787cd13ed95216c1a8f5badd004f
secretindex <caio.smelo04@gmail.com>
2025-11-25 10:14:25 -0300

    not used

--- app/api/token/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST() {
-  try {
-    
-  } catch (error) {
-    return NextResponse.json({ message: error, status: "fail" });
-  }
-}
