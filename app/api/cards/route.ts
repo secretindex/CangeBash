80988f1e734ecd439d86192ace2ecaa3efc9df54
secretindex <caio.smelo04@gmail.com>
2026-01-07 14:46:26 -0300

    tiflux panel

--- app/api/cards/route.ts
+++ app/api/cards/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    return NextResponse.json({ message: "hello", status: 200 });
-  } catch (error) {}
+    return NextResponse.json({ message: "hello", status: "ok" });
+  } catch (error) {
+    return NextResponse.json({ message: error, status: "fail" });
+  }
 }
