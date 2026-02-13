d5694405921fde42cb25b2aabcd80a63e71d1a64
secretindex <caio.smelo04@gmail.com>
2026-02-13 09:27:27 -0300

    liberate mensagem to non logged users

--- utils/supabase/proxy.ts
+++ utils/supabase/proxy.ts
@@ -42,6 +42,7 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/sign-in')
+    !request.nextUrl.pathname.startsWith('/mensagem')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
