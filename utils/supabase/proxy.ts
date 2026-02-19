f77e06f0e701fa91b3a08e3ff5cae6557eb89b04
secretindex <caio.smelo04@gmail.com>
2026-02-19 10:04:10 -0300

    fix: webhook not recieving messages

--- utils/supabase/proxy.ts
+++ utils/supabase/proxy.ts
@@ -42,7 +42,8 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/sign-in') &&
-    !request.nextUrl.pathname.startsWith('/mensagem')
+    !request.nextUrl.pathname.startsWith('/mensagem') &&
+    !request.nextUrl.pathname.startsWith("/api/webhook")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
